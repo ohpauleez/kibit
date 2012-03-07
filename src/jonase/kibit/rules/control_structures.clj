1a54d2945b1583bebb05dd40f16dc223262663c8
Paul deGrandis <paul.degrandis@gmail.com>
2012-03-07 18:39:52 -0500

    Some extra rules

--- src/jonase/kibit/rules/control_structures.clj
+++ src/jonase/kibit/rules/control_structures.clj
@@ -7,5 +7,6 @@
     (if (not ?x) ?y ?z) (if-not ?x ?y ?z)
     (when (not ?x) . ?y) (when-not ?x ?y)
     (if true ?x . ?y) ?x
-    (when true ?x) ?x}) ; Maybe this should be (do ?x)
+    (when true ?x) ?x ; Maybe this should be (do ?x)
+    (when-not true ?x) "Nothing - remove this dead code"})
 
