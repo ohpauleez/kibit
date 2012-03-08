4277c0c642d6eb71c0140a2594beb6600703c123
Paul deGrandis <paul.degrandis@gmail.com>
2012-03-07 21:41:55 -0500

    Snagged the filter rules from jonas

--- src/jonase/kibit/rules/misc.clj
+++ src/jonase/kibit/rules/misc.clj
@@ -5,4 +5,9 @@
 
     ;; mapcat
     (apply concat (apply map ?x ?y)) (mapcat ?x ?y)
-    (apply concat (map ?x . ?y)) (mapcat ?x . ?y)})
+    (apply concat (map ?x . ?y)) (mapcat ?x . ?y)
+
+    ;; filter
+    (filter (complement ?pred) ?coll) (remove ?pred ?coll) 
+    (filter #(not (?pred ?x)) ?coll) (remove ?pred ?coll)})
+
