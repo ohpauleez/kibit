35d02ecfac35931bd14be343f499b104a79b0141
Paul deGrandis <paul.degrandis@gmail.com>
2012-03-07 21:38:07 -0500

    Fixed misc rules

--- src/jonase/kibit/rules/misc.clj
+++ src/jonase/kibit/rules/misc.clj
@@ -1,8 +1,8 @@
 (ns jonase.kibit.rules.misc)
 
 (def rules
-  '{(apply str (interpose ?x ?y)) clojure.string/join
+  '{(apply str (interpose ?x ?y)) (clojure.string/join ?x ?y)
 
     ;; mapcat
-    (apply concat (apply map ?x ?y)) mapcat
-    (apply concat (map ?x . ?y)) mapcat})
+    (apply concat (apply map ?x ?y)) (mapcat ?x ?y)
+    (apply concat (map ?x . ?y)) (mapcat ?x . ?y)})
